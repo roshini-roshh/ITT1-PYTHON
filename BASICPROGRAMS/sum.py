f46df5b2e99934fd232dac899a628238e8b4ec65
roshini-roshh <roshniisivakumar@gmail.com>
2026-04-13 11:59:14 +0530

    Create sum.py

---
+++ BASICPROGRAMS/sum.py
@@ -0,0 +1,6 @@
+l=float(input("Enter the length:"))
+b=float(input("Enter the breadh:"))
+if(l>=1 and b<=100):
+   print("Area is:",l*b)
+else:
+   print("Enter the element according to the sonstraints")
