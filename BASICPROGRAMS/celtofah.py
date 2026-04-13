1477769d529341e4631969082795c6ca0f44b66c
roshini-roshh <roshniisivakumar@gmail.com>
2026-04-13 12:00:28 +0530

    Add Celsius to Fahrenheit conversion program

---
+++ BASICPROGRAMS/celtofah.py
@@ -0,0 +1,6 @@
+c=float(input("Enter the celcius value:"))
+if(c>= -50 and c<=100):
+    f=(c*(9//5))+32
+    print("The fahrenheit value is:",f)
+else:
+   print("enter the celcius value according to the constraints")
